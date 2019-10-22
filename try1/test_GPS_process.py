ca0960b6dd4c13d58b9fbb39fa6bd5b0ea8b6e85
ClearloveWei <weiyifei@sjtu.edu.cn>
2019-10-22 13:21:20 +0800

    first try

---
+++ try1/test_GPS_process.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+max_difference = 1
+in_txt = pd.read_csv('./1804010000.txt', sep='|', usecols=[0, 9, 10, 11])
+# in_txt = in_txt.sort_values(by='Noo')
+
+Noo_list = in_txt['Noo']
+Noo_list = Noo_list.drop_duplicates("first")
+Noo_list = Noo_list.sort_values()
+ans = in_txt.iloc[1]
+
+for item in Noo_list:
+    flag = True
+    temp = in_txt.query('Noo == %i' %item)
+    temp = temp.sort_values(by='8')
+    length = int(temp.size/4)
+    if(length<=1):
+        continue
+    for i in range(length):
+        first_row = temp.iloc[i]
+        if i+1 >=length:
+            break
+        second_row = temp.iloc[i+1]
+        if abs(first_row['jing']-second_row['jing']) > max_difference \
+                or abs(first_row['wei']-second_row['wei']) > max_difference:
+            if i+2>=length:
+                break
+            else:
+                third_row = temp.iloc[i+2]
+                if abs(first_row['jing']-third_row['jing']) > 2*max_difference \
+                        or abs(first_row['wei']-third_row['wei']) > 2*max_difference:
+                    flag = False
+                    break
+                else:
+                    temp.iloc[i+1]['jing'] = (first_row['jing']+third_row['jing'])/2
+                    temp.iloc[i + 1]['wei'] = (first_row['wei'] + third_row['wei'])/2
+    if flag:
+        ans = ans.append(temp)
+
+ans = ans[1:]
+ans.to_csv('./processed.csv', index=False)
